81b990501d613a5b6a13ce39880577a92da9069c
Robin <robin.s@kidsnote.com>
2020-06-19 14:59:02 +0900

    refactor(feat) : 무작위 멤버 로직을 domain 로직으로 이동
    repo 에서 무작위 멤버를 추출하던 로직이 들어있어서 로직을 전부 UseCase로 옮김

--- app/src/main/java/com/example/sampleapplication/random/RandomMemberRepoImpl.kt
+++ app/src/main/java/com/example/sampleapplication/random/RandomMemberRepoImpl.kt
@@ -3,10 +3,9 @@ package com.example.sampleapplication.random
 import com.classnote.domain.model.KidsnoteMember
 import com.classnote.domain.random.RandomMemberRepo
 
-class RandomMemberRepoImpl(db : KidsnoteMemberDB) : RandomMemberRepo {
-    private var index = 0
-    private val list = db.list.shuffled()
-    override val member get() = list[(index++ % list.size)]
+class RandomMemberRepoImpl(val db : KidsnoteMemberDB) : RandomMemberRepo {
+    override val totalMember get() = db.list.size
+    override fun getMember(index: Int) = db.list[index]
 }
 
 interface KidsnoteMemberDB {
--- domain/src/main/java/com/classnote/domain/random/RandomMemberUseCase.kt
+++ domain/src/main/java/com/classnote/domain/random/RandomMemberUseCase.kt
@@ -1,21 +1,30 @@
 package com.classnote.domain.random
 
-import com.classnote.domain.model.KidsnoteMember
 import com.classnote.domain.UseCase
 import com.classnote.domain.UseCaseInput
 import com.classnote.domain.UseCaseOutput
+import com.classnote.domain.model.KidsnoteMember
 
-class RandomMemberUseCase :
-    UseCase<RandomMemberRepo, UseCaseRandomMemberResult> {
+class RandomMemberUseCase : UseCase<RandomMemberRepo, UseCaseRandomMemberResult> {
+
+    private lateinit var shuffledIndex : List<Int>
+    private var lastIndex = 0
+
     override fun execute(input: RandomMemberRepo): UseCaseRandomMemberResult {
+        if (lastIndex == 0) {
+            shuffledIndex = (0 until input.totalMember).shuffled()
+        }
+        val index = shuffledIndex[lastIndex]
+        lastIndex = (lastIndex + 1) % shuffledIndex.size
         return object : UseCaseRandomMemberResult {
-            override val member = input.member
+            override val member = input.getMember(index)
         }
     }
 }
 
 interface RandomMemberRepo : UseCaseInput {
-    val member : KidsnoteMember
+    val totalMember : Int
+    fun getMember(index : Int) : KidsnoteMember
 }
 interface UseCaseRandomMemberResult : UseCaseOutput {
     val member : KidsnoteMember
